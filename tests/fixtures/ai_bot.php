e018f1309a940761b7e11936b1c3696f653451fb
Vitaliy <theyamshikov@yandex.ru>
2018-11-02 17:53:35 +0600

    make fixtures

---
+++ tests/fixtures/ai_bot.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	1 => [
+		'id' => '1',
+		'title' => 'Spam filter',
+	],
+];
---
+++ tests/fixtures/ai_class.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	1 => [
+		'id' => '1',
+		'bot_id' => '1',
+		'title' => 'spam',
+		'value' => '',
+	],
+	2 => [
+		'id' => '2',
+		'bot_id' => '1',
+		'title' => 'ham',
+		'value' => '',
+	],
+];
